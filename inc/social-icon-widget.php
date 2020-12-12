0f37c6622aab6d0f4895a9baf2daf6ff990a63b9
Patrick Sletvold <patricksletvold@hotmail.com>
2020-12-12 12:09:38 +0100

    Format PHP code with PHPCS

--- inc/social-icon-widget.php
+++ inc/social-icon-widget.php
@@ -1,70 +1,66 @@
 <?php
 
 // Creating the widget
-class Social_Icon_Widget extends WP_Widget
-{
-    public function __construct()
-    {
-        parent::__construct(
-// Base ID of your widget
-'Social_Icon_Widget',
+class Social_Icon_Widget extends WP_Widget {
+  public function __construct() {
+    parent::__construct(
+      // Base ID of your widget
+      'Social_Icon_Widget',
+      // Widget name will appear in UI
+      __( 'Social Icon Widget', 'social-icon-buttons' ),
+      // Widget description
+      [ 'description' => __( 'Social Icon Buttons Widget', 'social-icon-buttons' ) ]
+    );
+  }
 
-// Widget name will appear in UI
-__('Social Icon Widget', 'social-icon-buttons'),
+  // Creating widget front-end
+  // This is where the action happens
+  public function widget( $args, $instance ) {
+    $title = apply_filters( 'widget_title', $instance['title'] );
 
-// Widget description
-array( 'description' => __('Social Icon Buttons Widget', 'social-icon-buttons'), )
-);
+    // before and after widget arguments are defined by themes
+    echo $args['before_widget'];
+    if ( ! empty( $title ) ) {
+      echo $args['before_title'] . $title . $args['after_title'];
     }
 
-// Creating widget front-end
-// This is where the action happens
-public function widget($args, $instance)
-{
-    $title = apply_filters('widget_title', $instance['title']);
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
-    if (! empty($title)) {
-        echo $args['before_title'] . $title . $args['after_title'];
-    }
-
-// This is where you run the code and display the output
-echo '<div class="social-icons" style="max-width:100%;">';
+    // This is where you run the code and display the output
+    echo '<div class="social-icons" style="max-width:100%;">';
 
     $facebook = $instance['facebook'];
-    if (! empty($facebook)) {
-        echo '<a href="https://facebook.com/' . $facebook . '" class="facebook" target="_blank" rel="noopener" title="' . esc_html__('Facebook', 'social-icon-buttons') . '"><i class="social fa fa-facebook" aria-hidden="true"></i><span>' . esc_html__('Facebook Page', 'social-icon-buttons') . '</span></a>';
+    if ( ! empty( $facebook ) ) {
+      echo '<a href="https://facebook.com/' . $facebook . '" class="facebook" target="_blank" rel="noopener" title="' . esc_html__( 'Facebook', 'social-icon-buttons' ) . '"><i class="social fa fa-facebook" aria-hidden="true"></i><span>' . esc_html__( 'Facebook Page', 'social-icon-buttons' ) . '</span></a>';
     }
 
     $twitter = $instance['twitter'];
-    if (! empty($twitter)) {
-        echo '<a href="https://twitter.com/' . $twitter . '" class="twitter" target="_blank" rel="noopener" title="' . esc_html__('Twitter', 'social-icon-buttons') . '"><i class="social fa fa-twitter" aria-hidden="true"></i><span>' . esc_html__('Twitter Profile', 'social-icon-buttons') . '</span></a>';
+    if ( ! empty( $twitter ) ) {
+      echo '<a href="https://twitter.com/' . $twitter . '" class="twitter" target="_blank" rel="noopener" title="' . esc_html__( 'Twitter', 'social-icon-buttons' ) . '"><i class="social fa fa-twitter" aria-hidden="true"></i><span>' . esc_html__( 'Twitter Profile', 'social-icon-buttons' ) . '</span></a>';
     }
 
     $pinterest = $instance['pinterest'];
-    if (! empty($pinterest)) {
-        echo '<a href="https://pinterest.com/' . $pinterest . '" class="pinterest" target="_blank" rel="noopener" title="' . esc_html__('Pinterest', 'social-icon-buttons') . '"><i class="social fa fa-pinterest-p" aria-hidden="true"></i><span>' . esc_html__('Pinterest Profile', 'social-icon-buttons') . '</span></a>';
+    if ( ! empty( $pinterest ) ) {
+      echo '<a href="https://pinterest.com/' . $pinterest . '" class="pinterest" target="_blank" rel="noopener" title="' . esc_html__( 'Pinterest', 'social-icon-buttons' ) . '"><i class="social fa fa-pinterest-p" aria-hidden="true"></i><span>' . esc_html__( 'Pinterest Profile', 'social-icon-buttons' ) . '</span></a>';
     }
 
     $feedly = $instance['feedly'];
-    if (! empty($feedly)) {
-        echo '<a href="' . $feedly . '" class="feedly" target="_blank" rel="noopener" title="' . esc_html__('Feedly', 'social-icon-buttons') . '"><i class="social fa fa-rss" aria-hidden="true"></i><span>' . esc_html__('Feedly RSS Feed', 'social-icon-buttons') . '</span></a>';
+    if ( ! empty( $feedly ) ) {
+      echo '<a href="' . $feedly . '" class="feedly" target="_blank" rel="noopener" title="' . esc_html__( 'Feedly', 'social-icon-buttons' ) . '"><i class="social fa fa-rss" aria-hidden="true"></i><span>' . esc_html__( 'Feedly RSS Feed', 'social-icon-buttons' ) . '</span></a>';
     }
 
     $email = $instance['email'];
-    if (! empty($email)) {
-        echo '<a href="mailto:' . $email . '" class="email" target="_blank" rel="noopener" title="' . esc_html__('Email', 'social-icon-buttons') . '"><i class="social fa fa-envelope" aria-hidden="true"></i><span>' . esc_html__('Send email', 'social-icon-buttons') . '</span></a>';
+    if ( ! empty( $email ) ) {
+      echo '<a href="mailto:' . $email . '" class="email" target="_blank" rel="noopener" title="' . esc_html__( 'Email', 'social-icon-buttons' ) . '"><i class="social fa fa-envelope" aria-hidden="true"></i><span>' . esc_html__( 'Send email', 'social-icon-buttons' ) . '</span></a>';
     }
 
     $print = $instance['print'];
-    if (! empty($print)) {
-        echo '<a href="#" onclick="window.print()" class="print" target="_blank" rel="noopener" title="' . esc_html__('Print', 'social-icon-buttons') . '"><i class="social fa fa-print" aria-hidden="true" title="Print"></i><span>' . esc_html__('Print page', 'social-icon-buttons') . '</span></a>';
+    if ( ! empty( $print ) ) {
+      echo '<a href="#" onclick="window.print()" class="print" target="_blank" rel="noopener" title="' . esc_html__( 'Print', 'social-icon-buttons' ) . '"><i class="social fa fa-print" aria-hidden="true" title="Print"></i><span>' . esc_html__( 'Print page', 'social-icon-buttons' ) . '</span></a>';
     }
 
     echo '</div>';
 
     echo $args['after_widget'];
-/*
+    /*
     $social_icon_buttons = '
     <div class="social-icons">
         <a href="#" class="facebook" target="_blank" title="Facebook"><i class="social fa fa-facebook" aria-hidden="true"></i></a>
@@ -75,119 +71,118 @@ echo '<div class="social-icons" style="max-width:100%;">';
         <a href="#" onclick="window.print()" class="print" target="_blank"><i class="social fa fa-print" aria-hidden="true" title="Print"></i></a>
     </div>
     ';
-*/
-//echo $social_icon_buttons;
-}
+    */
+    //echo $social_icon_buttons;
+  }
 
-// Widget Backend
-public function form($instance)
-{
-    if (isset($instance[ 'title' ])) {
-        $title = $instance[ 'title' ];
+  // Widget Backend
+  public function form( $instance ) {
+    if ( isset( $instance['title'] ) ) {
+      $title = $instance['title'];
     } else {
-        $title = __('New title', 'social-icon-buttons');
+      $title = __( 'New title', 'social-icon-buttons' );
     }
 
-    if (isset($instance[ 'facebook' ])) {
-        $facebook = $instance[ 'facebook' ];
+    if ( isset( $instance['facebook'] ) ) {
+      $facebook = $instance['facebook'];
     } else {
-        $facebook = '';
+      $facebook = '';
     }
 
-    if (isset($instance[ 'twitter' ])) {
-        $twitter = $instance[ 'twitter' ];
+    if ( isset( $instance['twitter'] ) ) {
+      $twitter = $instance['twitter'];
     } else {
-        $twitter = '';
+      $twitter = '';
     }
 
-    if (isset($instance[ 'pinterest' ])) {
-        $pinterest = $instance[ 'pinterest' ];
+    if ( isset( $instance['pinterest'] ) ) {
+      $pinterest = $instance['pinterest'];
     } else {
-        $pinterest = '';
+      $pinterest = '';
     }
 
-    if (isset($instance[ 'feedly' ])) {
-        $feedly = $instance[ 'feedly' ];
+    if ( isset( $instance['feedly'] ) ) {
+      $feedly = $instance['feedly'];
     } else {
-        $feedly = '';
+      $feedly = '';
     }
 
-    if (isset($instance[ 'email' ])) {
-        $email = $instance[ 'email' ];
+    if ( isset( $instance['email'] ) ) {
+      $email = $instance['email'];
     } else {
-        $email = '';
+      $email = '';
     }
 
-    if (isset($instance[ 'print' ])) {
-        $print = $instance[ 'print' ];
+    if ( isset( $instance['print'] ) ) {
+      $print = $instance['print'];
     } else {
-        $print = '';
+      $print = '';
     }
-// Widget admin form
-?>
-	<p>
-		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'social-icon-buttons'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>"
-						type="text" value="<?php echo esc_attr($title); ?>" />
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('facebook'); ?>"><?php _e('Facebook:', 'social-icon-buttons'); ?></label>
-		<p>facebook.com/</p>
-		<input class="widefat" id="<?php echo $this->get_field_id('facebook'); ?>" name="<?php echo $this->get_field_name('facebook'); ?>"
-						type="text" value="<?php echo esc_attr($facebook); ?>" />
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('twitter'); ?>"><?php _e('Twitter:', 'social-icon-buttons'); ?></label>
-		<p>twitter.com/</p>
-		<input class="widefat" id="<?php echo $this->get_field_id('twitter'); ?>" name="<?php echo $this->get_field_name('twitter'); ?>"
-						type="text" value="<?php echo esc_attr($twitter); ?>" />
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('pinterest'); ?>"><?php _e('Pinterest:', 'social-icon-buttons'); ?></label>
-		<p>pinterest.com/</p>
-		<input class="widefat" id="<?php echo $this->get_field_id('pinterest'); ?>" name="<?php echo $this->get_field_name('pinterest'); ?>"
-						type="text" value="<?php echo esc_attr($pinterest); ?>" />
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('feedly'); ?>"><?php _e('Feedly:', 'social-icon-buttons'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('feedly'); ?>" name="<?php echo $this->get_field_name('feedly'); ?>"
-						type="text" value="<?php echo esc_attr($feedly); ?>" />
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('email'); ?>"><?php _e('Email:', 'social-icon-buttons'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('email'); ?>" name="<?php echo $this->get_field_name('email'); ?>"
-						type="text" value="<?php echo esc_attr($email); ?>" />
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('print'); ?>"><?php _e('Print:', 'social-icon-buttons'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('print'); ?>" name="<?php echo $this->get_field_name('print'); ?>"
-						type="text" value="<?php echo esc_attr($print); ?>" />
-	</p>
-	<?php
-}
 
-// Updating widget replacing old instances with new
-public function update($new_instance, $old_instance)
-{
-    $instance = array();
-    $instance['title'] = (! empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+    // Widget admin form
+    ?>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'social-icon-buttons' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>"
+                        type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'facebook' ); ?>"><?php _e( 'Facebook:', 'social-icon-buttons' ); ?></label>
+        <p>facebook.com/</p>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'facebook' ); ?>" name="<?php echo $this->get_field_name( 'facebook' ); ?>"
+                        type="text" value="<?php echo esc_attr( $facebook ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'twitter' ); ?>"><?php _e( 'Twitter:', 'social-icon-buttons' ); ?></label>
+        <p>twitter.com/</p>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'twitter' ); ?>" name="<?php echo $this->get_field_name( 'twitter' ); ?>"
+                        type="text" value="<?php echo esc_attr( $twitter ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'pinterest' ); ?>"><?php _e( 'Pinterest:', 'social-icon-buttons' ); ?></label>
+        <p>pinterest.com/</p>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'pinterest' ); ?>" name="<?php echo $this->get_field_name( 'pinterest' ); ?>"
+                        type="text" value="<?php echo esc_attr( $pinterest ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'feedly' ); ?>"><?php _e( 'Feedly:', 'social-icon-buttons' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'feedly' ); ?>" name="<?php echo $this->get_field_name( 'feedly' ); ?>"
+                        type="text" value="<?php echo esc_attr( $feedly ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'email' ); ?>"><?php _e( 'Email:', 'social-icon-buttons' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'email' ); ?>" name="<?php echo $this->get_field_name( 'email' ); ?>"
+                        type="text" value="<?php echo esc_attr( $email ); ?>" />
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'print' ); ?>"><?php _e( 'Print:', 'social-icon-buttons' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'print' ); ?>" name="<?php echo $this->get_field_name( 'print' ); ?>"
+                        type="text" value="<?php echo esc_attr( $print ); ?>" />
+    </p>
+    <?php
+  }
 
-    $instance['facebook'] = (! empty($new_instance['facebook'])) ? strip_tags($new_instance['facebook']) : '';
-    $instance['twitter'] = (! empty($new_instance['twitter'])) ? strip_tags($new_instance['twitter']) : '';
-    $instance['pinterest'] = (! empty($new_instance['pinterest'])) ? strip_tags($new_instance['pinterest']) : '';
-    $instance['feedly'] = (! empty($new_instance['feedly'])) ? strip_tags($new_instance['feedly']) : '';
-    $instance['email'] = (! empty($new_instance['email'])) ? strip_tags($new_instance['email']) : '';
-    $instance['print'] = (! empty($new_instance['print'])) ? strip_tags($new_instance['print']) : '';
+  // Updating widget replacing old instances with new
+  public function update( $new_instance, $old_instance ) {
+    $instance = [];
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+    $instance['facebook'] = ( ! empty( $new_instance['facebook'] ) ) ? strip_tags( $new_instance['facebook'] ) : '';
+    $instance['twitter'] = ( ! empty( $new_instance['twitter'] ) ) ? strip_tags( $new_instance['twitter'] ) : '';
+    $instance['pinterest'] = ( ! empty( $new_instance['pinterest'] ) ) ? strip_tags( $new_instance['pinterest'] ) : '';
+    $instance['feedly'] = ( ! empty( $new_instance['feedly'] ) ) ? strip_tags( $new_instance['feedly'] ) : '';
+    $instance['email'] = ( ! empty( $new_instance['email'] ) ) ? strip_tags( $new_instance['email'] ) : '';
+    $instance['print'] = ( ! empty( $new_instance['print'] ) ) ? strip_tags( $new_instance['print'] ) : '';
 
     return $instance;
-}
+  }
+
 } // Class wpb_widget ends here
 
 // Register and load the widget
-function social_icon_load_widget()
-{
-    register_widget('Social_Icon_Widget');
+function social_icon_load_widget() {
+  register_widget( 'Social_Icon_Widget' );
 }
-add_action('widgets_init', 'social_icon_load_widget');
+add_action( 'widgets_init', 'social_icon_load_widget' );
 
 ?>
--- social-icon-buttons.php
+++ social-icon-buttons.php
@@ -15,7 +15,7 @@ Includes
 ========
 */
 
-include_once plugin_dir_path(__FILE__).'/inc/social-icon-widget.php';
+require_once plugin_dir_path( __FILE__ ) . '/inc/social-icon-widget.php';
 
 /*
 =======
@@ -23,56 +23,55 @@ Filters
 =======
 */
 
-function add_social_icons($content)
-{
-    // Don't run the function unless we're on a page it applies to
-    if (! is_single()) {
-        return $content;
+function add_social_icons( $content ) {
+  // Don't run the function unless we're on a page it applies to
+  if ( ! is_single() ) {
+    return $content;
+  }
+
+  // TODO: Find a better solution.
+  if ( function_exists( 'is_amp_endpoint' ) ) {
+    if ( is_amp_endpoint() ) {
+      return $content;
     }
+  }
 
-    // TODO: Find a better solution.
-    if ( function_exists( 'is_amp_endpoint' ) ) {
-		if ( is_amp_endpoint() ) {
-			return $content;
-		}
-	}
+  global $post;
 
-    global $post;
+  $urlCurrentPage = get_permalink( $post->ID );
+  $strPageTitle = get_the_title( $post->ID );
+  $shareText = urlencode( html_entity_decode( $strPageTitle . ' - Multitek', ENT_COMPAT, 'UTF-8' ) );
+  $emailTitle = str_replace( '&', '%26', $strPageTitle );
+  $emailContent = str_replace( '+', '%20', $strPageTitle );
 
-    $urlCurrentPage = get_permalink($post->ID);
-    $strPageTitle = get_the_title($post->ID);
-    $shareText = urlencode(html_entity_decode($strPageTitle . ' - Multitek', ENT_COMPAT, 'UTF-8'));
-    $emailTitle = str_replace('&', '%26', $strPageTitle);
-    $emailContent = str_replace('+', '%20', $strPageTitle);
+  if ( has_post_thumbnail() ) {
+    // get the featured image
+    $urlPostThumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+    $urlPostThumb = $urlPostThumb[0];
+  }
+  // no featured image set
+  else {
+    // use the pinterest default
+    $urlPostThumb = 'https://cdn.multitek.no/img/multitek-logo/logo_gjennomsiktig_facebook.png';
+  }
 
-    if (has_post_thumbnail()) {
-        // get the featured image
-        $urlPostThumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
-        $urlPostThumb = $urlPostThumb[0];
-    }
-    // no featured image set
-    else {
-        // use the pinterest default
-        $urlPostThumb = 'https://cdn.multitek.no/img/multitek-logo/logo_gjennomsiktig_facebook.png';
-    }
-
-    $share_buttons = '
+  $share_buttons = '
 	<div class="social-icons">
-		<a href="http://www.facebook.com/sharer.php?u=' . $urlCurrentPage . '" class="facebook" target="_blank" rel="noopener" title="' . esc_html__('Facebook', 'social-icon-buttons') . '"><i class="social fa fa-facebook" aria-hidden="true"></i><span>' . esc_html__('Share using Facebook', 'social-icon-buttons') . '</span></a>
-		<a href="http://twitter.com/share?url=' . $urlCurrentPage . '&text=' . $shareText . '" class="twitter" target="_blank" rel="noopener" title="' . esc_html__('Twitter', 'social-icon-buttons') . '"><i class="social fa fa-twitter" aria-hidden="true"></i><span>' . esc_html__('Share using Twitter', 'social-icon-buttons') . '</span></a>
-		<a href="http://pinterest.com/pin/create/bookmarklet/?is_video=false&url=' . $urlCurrentPage . '&media=' . $urlPostThumb . '&description=' . $strPageTitle . '" class="pinterest" target="_blank" rel="noopener" title="' . esc_html__('Pinterest', 'social-icon-buttons') . '"><i class="social fa fa-pinterest-p" aria-hidden="true"></i><span>' . __('Share using Pinterest', 'social-icon-buttons') . '</span></a>
-		<a href="http://reddit.com/submit?url=' . $urlCurrentPage  . '&amp;title=' . $strPageTitle . '" class="reddit" target="_blank" rel="noopener" title="' . esc_html__('Reddit', 'social-icon-buttons') . '"><i class="social fa fa-reddit-alien" aria-hidden="true"></i><span>' . esc_html__('Share using Reddit', 'social-icon-buttons') . '</span></a>
-		<a href="https://getpocket.com/save?url=' . $urlCurrentPage  . '&amp;title=' . $strPageTitle . '" class="pocket" target="_blank" rel="noopener" title="' . esc_html__('Pocket', 'social-icon-buttons') . '"><i class="social fa fa-get-pocket" aria-hidden="true"></i><span>' . esc_html__('Save to Pocket', 'social-icon-buttons') . '</span></a>
-		<a href="https://telegram.me/share/url?url=' . $urlCurrentPage  . '&amp;text=' . $shareText . '" class="telegram" target="_blank" title="' . esc_html__('Telegram', 'social-icon-buttons') . '"><i class="social fa fa-telegram" aria-hidden="true"></i><span>' . esc_html__('Share using Telegram', 'social-icon-buttons') . '</span></a>
-		<a href="mailto:?subject=' . $emailTitle . '&amp;body=' . $emailContent . '%20' . $urlCurrentPage  . '" class="email" target="_blank" rel="noopener" title="' . esc_html__('Email', 'social-icon-buttons') . '"><i class="social fa fa-envelope" aria-hidden="true"></i><span>' . esc_html__('Share using email', 'social-icon-buttons') . '</span></a>
-		<a href="#" onclick="window.print()" class="print" target="_blank" rel="noopener" title="Print"><i class="social fa fa-print" aria-hidden="true" title="' . esc_html__('Print', 'social-icon-buttons') . '"></i><span>' . esc_html__('Print page', 'social-icon-buttons') . '</span></a>
+		<a href="http://www.facebook.com/sharer.php?u=' . $urlCurrentPage . '" class="facebook" target="_blank" rel="noopener" title="' . esc_html__( 'Facebook', 'social-icon-buttons' ) . '"><i class="social fa fa-facebook" aria-hidden="true"></i><span>' . esc_html__( 'Share using Facebook', 'social-icon-buttons' ) . '</span></a>
+		<a href="http://twitter.com/share?url=' . $urlCurrentPage . '&text=' . $shareText . '" class="twitter" target="_blank" rel="noopener" title="' . esc_html__( 'Twitter', 'social-icon-buttons' ) . '"><i class="social fa fa-twitter" aria-hidden="true"></i><span>' . esc_html__( 'Share using Twitter', 'social-icon-buttons' ) . '</span></a>
+		<a href="http://pinterest.com/pin/create/bookmarklet/?is_video=false&url=' . $urlCurrentPage . '&media=' . $urlPostThumb . '&description=' . $strPageTitle . '" class="pinterest" target="_blank" rel="noopener" title="' . esc_html__( 'Pinterest', 'social-icon-buttons' ) . '"><i class="social fa fa-pinterest-p" aria-hidden="true"></i><span>' . __( 'Share using Pinterest', 'social-icon-buttons' ) . '</span></a>
+		<a href="http://reddit.com/submit?url=' . $urlCurrentPage . '&amp;title=' . $strPageTitle . '" class="reddit" target="_blank" rel="noopener" title="' . esc_html__( 'Reddit', 'social-icon-buttons' ) . '"><i class="social fa fa-reddit-alien" aria-hidden="true"></i><span>' . esc_html__( 'Share using Reddit', 'social-icon-buttons' ) . '</span></a>
+		<a href="https://getpocket.com/save?url=' . $urlCurrentPage . '&amp;title=' . $strPageTitle . '" class="pocket" target="_blank" rel="noopener" title="' . esc_html__( 'Pocket', 'social-icon-buttons' ) . '"><i class="social fa fa-get-pocket" aria-hidden="true"></i><span>' . esc_html__( 'Save to Pocket', 'social-icon-buttons' ) . '</span></a>
+		<a href="https://telegram.me/share/url?url=' . $urlCurrentPage . '&amp;text=' . $shareText . '" class="telegram" target="_blank" title="' . esc_html__( 'Telegram', 'social-icon-buttons' ) . '"><i class="social fa fa-telegram" aria-hidden="true"></i><span>' . esc_html__( 'Share using Telegram', 'social-icon-buttons' ) . '</span></a>
+		<a href="mailto:?subject=' . $emailTitle . '&amp;body=' . $emailContent . '%20' . $urlCurrentPage . '" class="email" target="_blank" rel="noopener" title="' . esc_html__( 'Email', 'social-icon-buttons' ) . '"><i class="social fa fa-envelope" aria-hidden="true"></i><span>' . esc_html__( 'Share using email', 'social-icon-buttons' ) . '</span></a>
+		<a href="#" onclick="window.print()" class="print" target="_blank" rel="noopener" title="Print"><i class="social fa fa-print" aria-hidden="true" title="' . esc_html__( 'Print', 'social-icon-buttons' ) . '"></i><span>' . esc_html__( 'Print page', 'social-icon-buttons' ) . '</span></a>
 	</div>
 	';
-    $returnContent = $share_buttons . $content . $share_buttons;
+  $returnContent = $share_buttons . $content . $share_buttons;
 
-    return $returnContent;
+  return $returnContent;
 }
-add_filter('the_content', 'add_social_icons');
+add_filter( 'the_content', 'add_social_icons' );
 
 /*
 =======
@@ -80,9 +79,7 @@ Actions
 =======
 */
 
-function add_icon_stylesheet()
-{
-    wp_register_style('social-icon-buttons', plugin_dir_url(__FILE__) . '/inc/social-icons.css');
-    wp_enqueue_style('social-icon-buttons');
+function add_icon_stylesheet() {    wp_register_style( 'social-icon-buttons', plugin_dir_url( __FILE__ ) . '/inc/social-icons.css' );
+  wp_enqueue_style( 'social-icon-buttons' );
 }
-add_action('wp_enqueue_scripts', 'add_icon_stylesheet');
+add_action( 'wp_enqueue_scripts', 'add_icon_stylesheet' );
